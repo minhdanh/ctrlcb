c03a75a4a2ba4f0f08e996d7fd00f945412f398a
minhdanh <minhdanh@users.noreply.github.com>
2021-06-15 10:25:15 +0700

    Add more test cases

--- cmd/ctrlcb-paste/main_test.go
+++ cmd/ctrlcb-paste/main_test.go
@@ -86,24 +86,25 @@ func TestCopyFile_AbsolutePath(t *testing.T) {
 	}
 }
 
-func TestCopyFile_RelativePath(t *testing.T) {
+func TestCopyFile_RelativePathTarget(t *testing.T) {
 	sourceWorkingDirectory := t.TempDir()
 	// sourcePath := filepath.Join(sourceWorkingDirectory, "a/b/c")
 	// os.MkdirAll(sourcePath, os.ModePerm)
-	tmpFile, err := ioutil.TempFile(sourceWorkingDirectory, "test-")
+	tmpFileT, err := ioutil.TempFile(sourceWorkingDirectory, "test-")
 	if err != nil {
 		log.Fatal("Cannot create temporary file", err)
 	}
 
 	targetWorkingDirectory := t.TempDir()
 	os.Chdir(targetWorkingDirectory)
-	relativeFilePath, err := filepath.Rel(targetWorkingDirectory, tmpFile.Name())
+	// relativeFilePathT is relative from target directory
+	relativeFilePathT, err := filepath.Rel(targetWorkingDirectory, tmpFileT.Name())
 	if err != nil {
-		log.Fatal("Cannot create temporary file", err)
+		log.Fatal("Cannot create relative path", err)
 	}
 
 	clipboardContent := "# ctrlcb"
-	clipboardContent, _ = functions.AddClipboardItem(clipboardContent, sourceWorkingDirectory, relativeFilePath)
+	clipboardContent, _ = functions.AddClipboardItem(clipboardContent, sourceWorkingDirectory, relativeFilePathT)
 
 	expected := 1
 	actual := processItems(clipboardContent, targetWorkingDirectory, false, false)
@@ -147,8 +148,71 @@ func TestCopyFile_RelativePath(t *testing.T) {
 	}
 
 	// check if relative path exist in targetWorkingDirectory
-	_, err = os.Stat(filepath.Join(targetWorkingDirectory, strings.TrimLeft(relativeFilePath, "./")))
+	_, err = os.Stat(filepath.Join(targetWorkingDirectory, strings.TrimLeft(relativeFilePathT, "./")))
 	if os.IsNotExist(err) {
-		t.Fatalf("%v not copied to %v", relativeFilePath, targetWorkingDirectory)
+		t.Fatalf("%v not copied to %v", relativeFilePathT, targetWorkingDirectory)
+	}
+}
+
+func TestCopyFile_RelativePathSource(t *testing.T) {
+	sourceWorkingDirectory := t.TempDir()
+	// sourcePath := filepath.Join(sourceWorkingDirectory, "a/b/c")
+	// os.MkdirAll(sourcePath, os.ModePerm)
+	tmpFileS, err := ioutil.TempFile(sourceWorkingDirectory, "test-")
+	if err != nil {
+		log.Fatal("Cannot create temporary file", err)
+	}
+
+	// relativeFilePathS is relative from source directory
+	relativeFilePathS, err := filepath.Rel(sourceWorkingDirectory, tmpFileS.Name())
+	if err != nil {
+		log.Fatal("Cannot create relative path", err)
+	}
+	log.Print(relativeFilePathS)
+
+	targetWorkingDirectory := t.TempDir()
+	os.Chdir(targetWorkingDirectory)
+
+	clipboardContent := "# ctrlcb"
+	clipboardContent, _ = functions.AddClipboardItem(clipboardContent, sourceWorkingDirectory, relativeFilePathS)
+
+	expected := 1
+	actual := processItems(clipboardContent, targetWorkingDirectory, false, false)
+	if actual != expected {
+		t.Fatalf("want %v, got %v", expected, actual)
+	}
+
+	// should not overwrite file created in previous assertion
+	expected = 0
+	actual = processItems(clipboardContent, targetWorkingDirectory, false, false)
+	if actual != expected {
+		t.Fatalf("want %v, got %v", expected, actual)
+	}
+
+	// should overwrite file created in previous assertion
+	expected = 1
+	actual = processItems(clipboardContent, targetWorkingDirectory, false, true)
+	if actual != expected {
+		t.Fatalf("want %v, got %v", expected, actual)
+	}
+
+	// should not copy with source path with -k as there's no directory in path
+	expected = 0
+	actual = processItems(clipboardContent, targetWorkingDirectory, true, false)
+	if actual != expected {
+		t.Fatalf("want %v, got %v", expected, actual)
+	}
+
+	// should overwrite file created in previous assertion
+	expected = 1
+	actual = processItems(clipboardContent, targetWorkingDirectory, true, true)
+	if actual != expected {
+		t.Fatalf("want %v, got %v", expected, actual)
+	}
+
+	// check if relative path exist in targetWorkingDirectory
+	_, err = os.Stat(filepath.Join(targetWorkingDirectory, strings.TrimLeft(relativeFilePathS, "./")))
+	if os.IsNotExist(err) {
+		t.Fatalf("%v not copied to %v", relativeFilePathS, targetWorkingDirectory)
 	}
 }
